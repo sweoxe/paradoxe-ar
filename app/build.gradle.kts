f316250fd37522ead02177f772abf7f92159f5e8
sweoxe <sweoxe@gmail.com>
2026-05-09 13:43:58 +0300

    feat: Add Google Material dependency
    Includes the Google Material library to the project dependencies. This will allow for the use of Material Design components and styling throughout the application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
+    implementation(libs.google.material)
 
     // Hilt
     implementation(libs.hilt.android)
